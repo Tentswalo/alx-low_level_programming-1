59dd70cc18278f0c99bd3615de9a0e8379a7865d
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-02 22:38:40 +0000

    Changed print message om 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int ll;
 	float f;
 
-	printf("Size of a char: %lu bytes(s)\n", (unsigned int)sizeof(c));
-	printf("Size of an int: %lu bytes(s)\n", (unsigned int)sizeof(i));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned int)sizeof(l));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned int)sizeof(ll));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned int)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned int)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned int)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned int)sizeof(f));
 	return (0);
 }
