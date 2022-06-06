d43c8447fe3a92aeabd2018936a372a59393b6cb
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-06 20:48:20 +0000

    Prinf for numbers
    -Create 5-print_numbers.c program
    -Delete compiled executable files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int set;
+
+	for (set = '0'; set <= '9'; set++)
+	{
+		printf("%d", set);
+	}
+	return (0);
+}
