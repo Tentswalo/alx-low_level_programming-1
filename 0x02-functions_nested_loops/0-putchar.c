2601a0991d1dd07ec04dd4af7a4f95c01be39abf
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-08 08:40:36 +0000

    Initialising Project
    -Create README.md file
    -Create main.h file and include _putcher function
    -Create 0-putchar.c program to print _putcher

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *_putchar - Prints characters
+ *Return: Always 0 for success
+ */
+
+int main(void)
+{
+	char p[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+This directory contains the followign files from the 0x02-functions_nested_loops project by ALX:
+
+## 0-putchar.c
+
+
+## 1-alphabet.c
