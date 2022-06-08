66eadc310f1e36046027a9bb09407899160c462b
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-08 15:51:11 +0000

    Determining Alphabet Case
    -Create _islower function to determine alphabet case
    -Update main.h

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - determines if the charcter c is lower case
+ * @c: The character to check
+ *
+ * Return: On success 1
+ * On error: 0 is returned
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+}
