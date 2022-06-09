26cb391dfa7257c47a081348d55f856530e10b19
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-09 02:08:06 +0000

    Create function to print last digit of integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,15 @@
 
 /**
  * print_last_digit - print last digit
+ * @c: variable to analyse
  *
  * Return: On success 0
  */
+
+int print_last_digit(int c)
+{
+	int i = c % 10;
+
+	_putchar(i);
+return (i);
+}
