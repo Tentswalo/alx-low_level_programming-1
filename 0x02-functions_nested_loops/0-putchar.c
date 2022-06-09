b3ca14d743bdc92de7cd7912376efc9b68220f0a
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-09 00:47:12 +0000

    Change character array parametres

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,12 @@ int main(void)
 	char p[9] = "_putchar";
 	int i;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 9; i++)
 	{
-		_putchar(p[i]);
+		_putchar(i[p]);
 	}
+
 	_putchar('\n');
+
 	return (0);
 }
