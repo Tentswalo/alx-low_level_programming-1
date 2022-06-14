e205c780cfe00d6c5335e2bc46c965d2ae3288a9
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-14 00:13:02 +0000

    Create function to update derefenced pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Changes value of varaible that pointer points to
+ * @n: Pointer to variable
+ *
+ * Return: None. Always void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
