6b8ef5177a947fe7afe55e488a6eed45e8d81ffe
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-21 17:02:16 +0000

    Rewrite code to print missing lines

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,20 +2,16 @@
 
 /**
  * jack_bauer - Prints every minute of jack_bauer starting from 00:00 to 23:59
- *
  * Return: Always void
  */
 
 void jack_bauer(void)
 {
-	int i;
-	int j;
-	int k;
-	int l;
+	int a, b, c, d, i, j, k, l;
 
 	for (i = '0'; i <= '2'; i++)
 	{
-		for (j = '0'; j <= '3'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			for (k = '0'; k <= '5'; k++)
 			{
@@ -28,6 +24,23 @@ void jack_bauer(void)
 					_putchar(l);
 					_putchar('\n');
 				}
+			}
+		}
+	}
+	for (b = '0'; b <= '3'; b++)
+	{
+		for (c = '0'; c <= '5'; c++)
+		{
+			for (d = '0'; d <= '9'; d++)
+			{
+				a = '2';
+
+				_putchar(a);
+				_putchar(b);
+				_putchar(':');
+				_putchar(c);
+				_putchar(d);
+				_putchar('\n');
 			}
 		}
 	}
