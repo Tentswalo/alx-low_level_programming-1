6859cabc09eb6f6bd42773de5a28464f5319d155
Chiloane-lt <chiloane.lt@hotmail.com>
2022-06-28 14:28:49 +0000

    Change return value if unsuccessful

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,12 +22,19 @@ char *create_array(unsigned int size, char c)
 
 		arr = malloc(sizeof(char) * size);
 
-		for (i = 0; i < size; i++)
+		if (arr == NULL)
 		{
-			arr[i] = c;
+			return (NULL);
 		}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				arr[i] = c;
+			}
 
-		return (arr);
+			return (arr);
+		}
 
 	}
 
